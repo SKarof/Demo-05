3168f28da6f4c7c884b415bf885fef9887527083
proxxy <skarof565@gmail.com>
2025-04-08 14:11:35 +0300

    Add new input password_confirm and create values to registration-handler

--- components/sections/registration/register_form.php
+++ components/sections/registration/register_form.php
@@ -1,4 +1,4 @@
-<form method="post" action="backend/registration-handler.php">
+<form method="post" action="src/actions/registration-handler.php">
     <div class="form-floating mb-3">
         <input type="email" class="form-control" name="email" id="floatingInputEmail" placeholder="name@example.com" required>
         <label for="floatingInputEmail">Email</label>
@@ -11,9 +11,13 @@
         <input type="password" class="form-control" name="password" id="floatingInputPassword" placeholder="Пароль" required>
         <label for="floatingInputPassword">Пароль</label>
     </div>
-    <div class="input-group input-group-lg mb-5">
+    <div class="form-floating mb-3">
+        <input type="password" class="form-control" name="password_confirm" id="floatingInputPasswordConfirm" placeholder="Пароль" required>
+        <label for="floatingInputPassword">Повторите пароль</label>
+    </div>
+    <div class="input-group mb-5">
         <span class="input-group-text" id="inputGroup-sizing-lg">+7</span>
-        <input type="tel" class="form-control" name="phone" aria-label="Поле для ввода номера телефона" aria-describedby="inputGroup-sizing-lg">
+        <input type="tel" class="form-control py-3" name="phone" aria-label="Поле для ввода номера телефона" placeholder="Номер телефона (Необязательно)">
     </div>
     <button type="submit" class="btn btn-primary btn-lg w-100 p-3 mb-2">Зарегистрироваться</button>
     <p>У меня уже есть аккаунт <a href="login.php">Войти</a>.</p>
---
+++ src/actions/registration-handler.php
@@ -0,0 +1,7 @@
+<?php
+    $email = $_POST['email']; 
+    $name = $_POST['name'];
+    $tel = $_POST['phone'];
+    $password = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+?>

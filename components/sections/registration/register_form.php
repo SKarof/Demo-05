d7f942f51350cf6f7f1d5234c9284ad290ba3914
proxxy <skarof565@gmail.com>
2025-04-08 13:10:53 +0300

    Create registration form

---
+++ components/sections/registration/register_form.php
@@ -0,0 +1,20 @@
+<form>
+    <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+        <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="floatingInput" placeholder="Иван" required>
+        <label for="floatingInput">Ваше имя</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingInput" placeholder="Пароль" required>
+        <label for="floatingInput">Пароль</label>
+    </div>
+    <div class="input-group input-group-lg mb-5">
+        <span class="input-group-text" id="inputGroup-sizing-lg">+7</span>
+        <input type="tel" class="form-control" aria-label="Поле для ввода номера телефона" aria-describedby="inputGroup-sizing-lg">
+    </div>
+    <button type="submit" class="btn btn-primary btn-lg w-100 p-3 mb-2">Зарегистрироваться</button>
+    <p>У меня уже есть аккаунт <a href="login.php">Войти</a>.</p>
+</form>
--- register.php
+++ register.php
@@ -2,8 +2,18 @@
 $page_title = 'Регистрация'; // Задаем заголовок страницы
 include 'components/header.php';
 ?>
-<body>
-    <form>
-
-    </form>
+<body class="bg-body-tertiary">
+    <main>
+        <section class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <h1 class="text-center display-4 mb-3">Регистрация</h1>
+                    <div class="registrationblock">
+                        <?php require('components/sections/registration/register_form.php') ?> 
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
+<?php include 'components/footer.php' ?>
